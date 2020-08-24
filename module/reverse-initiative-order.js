a16b9e3f72bb5dd0a30839b3943956db529bec4a
Åke Wivänge <ake.wivange@pagero.com>
2020-08-24 17:19:17 +0200

    Rename fix bug and add some git workflow.

--- module/reverse-initiative-order.js
+++ module/reverse-initiative-order.js
@@ -1,14 +1,15 @@
-class InitiativeOrder {
+class ReverseInitiativeOrder {
 
-	static async setup() {
-        Combat.prototype.setupTurns = setupTurns;
+    static async setup() {
+        console.log('reverse-initiative-order | Initializing Reverse Initiative Order module');
+        Combat.prototype.setupTurns = ReverseInitiativeOrder.setupTurns;
     }
-    
-    setupTurns() {
+
+    static setupTurns() {
         const scene = game.scenes.get(this.data.scene);
         const players = game.users.players;
         // Populate additional data for each combatant
-        let turns = this.data.combatants
+        const turns = this.data.combatants
             .map((c) => {
                 c.token = scene.getEmbeddedEntity("Token", c.tokenId);
                 if (!c.token) return c;
@@ -20,18 +21,18 @@ class InitiativeOrder {
                 c.visible = c.owner || !c.hidden;
                 return c;
             })
-            .filter((c) => c.token);
-        // Sort turns into initiative order: (1) initiative, (2) name, (3) tokenId
-        turns = turns.sort((a, b) => {
-            const ia = Number.isFinite(a.initiative) ? a.initiative : 9999;
-            const ib = Number.isFinite(b.initiative) ? b.initiative : 9999;
-            let ci = ib - ia;
-            if (ci !== 0) return ci;
-            let [an, bn] = [a.token.name || "", b.token.name || ""];
-            let cn = an.localeCompare(bn);
-            if (cn !== 0) return cn;
-            return a.tokenId - b.tokenId;
-        });
+            .filter((c) => c.token)
+            // Sort turns into initiative order: (1) initiative, (2) name, (3) tokenId
+            .sort((a, b) => {
+                const ia = Number.isFinite(a.initiative) ? a.initiative : 9999;
+                const ib = Number.isFinite(b.initiative) ? b.initiative : 9999;
+                const ci = ia - ib;
+                if (ci !== 0) return ci;
+                let [an, bn] = [a.token.name || "", b.token.name || ""];
+                let cn = an.localeCompare(bn);
+                if (cn !== 0) return cn;
+                return a.tokenId - b.tokenId;
+            });
         // Ensure the current turn is bounded
         this.data.turn = Math.clamped(this.data.turn, 0, turns.length - 1);
         this.turns = turns;
@@ -41,4 +42,4 @@ class InitiativeOrder {
     }
 }
 
-Hooks.on('init', InitiativeOrder.setup);
+Hooks.on('init', ReverseInitiativeOrder.setup);
